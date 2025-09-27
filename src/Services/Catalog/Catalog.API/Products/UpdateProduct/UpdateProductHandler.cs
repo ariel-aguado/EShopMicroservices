6f4544734d9b18a609028b02b5a4c8ab096516bc
Ariel Aguado Albear <ariel.841207@gmail.com>
2025-09-27 18:50:45 -0300

    Develop Catalog API - :sparkles: MediatR Pipeline Behaviours and Fluent Validation Library - :sparkles: Add Cross-cutting Concerns into BuildingBlocks Class Library for all services - :sparkles: Cross-cutting Concerns Validate with AbstractValidator using Fluent Validation - :sparkles: Validation Pipeline Behaviour with MediatR using Fluent Validation Library - :sparkles: Remove Manual Validator and Test Validation Pipeline Behaviour with MediatR - :sparkles: Develop CRUD Command Validators with Fluent Validation Pipeline Behaviour - :sparkles: Global Handling Exceptions in ASP.NET Core with app.UseExceptionHandler - :sparkles: BuildingBlocks Handling Exceptions Generic Way w app.UseExceptionHandler - :sparkles: Global Exception Handling with IExceptionHandler interface in Asp.net Core - :sparkles: Register Custom Exception Handling into Asp.net with AddExceptionHandler method - :sparkles: Logging Behavior in MediatR Pipeline for Cross-cutting concerns - :sparkles: Seeding CatalogDb with Marten Initial Baseline Data (IInitialData interface) - :sparkles: Develop Pagination for GetProducts Query with ToPagedListAsync extention method - :sparkles: Health Check for Catalog Microservices with PostgresDB - :sparkles: Containerize Catalog.API microservices with Docker - :sparkles: Orchestrate Catalog.API microservices with Docker-compose file

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,29 @@
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, string Description, string ImageFile, decimal Price, List<string> Category, int Count, bool IsAvailable) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException(command.Id);
+        }
+        
+        product.Name = command.Name;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+        product.Category = command.Category;
+        product.Count = command.Count;
+        product.IsAvailable = command.IsAvailable;
+        
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new UpdateProductResult(true);
+    }
+}
